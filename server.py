e2b99d588f97388a753ba8425587c80b04ced385
jonbond5 <ubuntu@ip-172-31-19-113.us-west-2.compute.internal>
2016-09-25 12:35:59 +0000

    this

--- server.py
+++ server.py
@@ -24,6 +24,10 @@ def fonts(path):
 def js(path):
     return send_from_directory('js',path)
 
+@app.route("/plugin/<path:path>",methods=['GET'])
+def plugin(path):
+    return send_from_directory('plugin',path)
+
 # Return home screen
 @app.route("/", methods=['GET','POST'])
 def index():
@@ -32,7 +36,9 @@ def index():
 # Return text field
 @app.route('/text', methods=['GET','POST'])
 def text():
+    points = 0
     return render_template('text_area.html',
+        points = points,
         success = 'none',
         stdout = 'none',
         error  = 'none')
@@ -40,6 +46,7 @@ def text():
 # Check if code is right
 @app.route('/check',methods=['GET','POST'])
 def checkCode():
+    points = 0
     code     = str(request.form['value'])
     language = str(request.form['lang'])
     problem  = str(request.form['prob'])
@@ -49,9 +56,20 @@ def checkCode():
     else:
         error = False
     return render_template('text_area.html',
+        points = points,
         success = output['correct'],
         stdout = output['stdout'],
         error  = error)
+
+@app.route('/upvote')
+def upvote():
+    print 'up'
+    return render_template('text_area.html')
+
+@app.route('/downvote')
+def downvote():
+    print 'down'
+    return render_template('text_area.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port='8080')
